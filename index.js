02ae68183e25285bf9e48eb36485d3f8a93d9b10
Isaac <isaac.lin.pnc@gmail.com>
2023-01-17 17:01:18 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Add your code here
+
+const submitData = function(userName, userEmail) {
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const body = document.querySelector('body')
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <p>Username: ${data.name}</p>
+        <p>Email: ${data.email}</p>
+        `
+        body.appendChild(div)
+    })
+    .catch(error => {
+        alert("Error!");
+        console.log(error.message)
+    })
+}
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     });
