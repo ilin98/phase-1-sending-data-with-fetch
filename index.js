ccdfb6944b521a9f7ee84b59dd73da6dc680790f
Isaac <isaac.lin.pnc@gmail.com>
2023-01-18 09:37:14 -0500

    finished lab with all tests passing

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const submitData = function(userName, userEmail) {
     return fetch("http://localhost:3000/users",{
         method: "POST",
@@ -13,39 +12,15 @@ const submitData = function(userName, userEmail) {
     })
     .then(response => response.json())
     .then(data => {
-        const body = document.querySelector('body')
-        const div = document.createElement('div')
-        div.innerHTML = `
-        <p>Username: ${data.name}</p>
-        <p>Email: ${data.email}</p>
+        const newUser = document.createElement('div')
+        newUser.innerHTML = `
+        <p>id: ${data.id}</p>
         `
-        body.appendChild(div)
+        document.querySelector("body").append(newUser);
+
     })
     .catch(error => {
         alert("Error!");
-        console.log(error.message)
+        document.querySelector("body").append(error.message)
     })
 }
-
-
-// const formData = {
-//     dogName: "Byron",
-//     dogBreed: "Poodle",
-//   };
-
-//   const configurationObject = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Accept": "application/json",
-//     },
-//     body: JSON.stringify(formData),
-//   };
-
-//   fetch("http://localhost:3000/dogs", configurationObject)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (object) {
-//       console.log(object);
-//     });
